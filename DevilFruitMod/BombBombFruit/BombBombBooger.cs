abfba01b28a94d9f8486b1ba40505ab97e92f5ff
Ellie Moffett <andy.t.moffett2000@gmail.com>
2021-11-14 22:13:03 -0500

    WU p2

---
+++ DevilFruitMod/BombBombFruit/BombBombBooger.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.GameContent.Achievements;
+using Terraria.ID;
+
+namespace DevilFruitMod.BombBombFruit
+{
+    public class BombBombBooger : ModProjectile
+    {
+        public override void SetDefaults()
+        {
+            projectile.width = 12;
+            projectile.height = 12;
+            projectile.aiStyle = 1;
+            projectile.friendly = true;
+            projectile.ranged = true;
+            projectile.alpha = 128;
+            projectile.timeLeft = 300;
+
+			projectile.penetrate = -1;
+		}
+
+
+		public override void ModifyHitNPC(NPC target, ref int damage, ref float knockback, ref bool crit, ref int hitDirection)
+		{
+			projectile.timeLeft = 3;
+			// Vanilla explosions do less damage to Eater of Worlds in expert mode, so we will too.
+			if (Main.expertMode)
+			{
+				if (target.type >= NPCID.EaterofWorldsHead && target.type <= NPCID.EaterofWorldsTail)
+				{
+					damage /= 5;
+				}
+			}
+		}
+
+		public override bool OnTileCollide(Vector2 oldVelocity)
+		{
+			projectile.timeLeft = 3;
+			return false;
+		}
+
+
+		public override void AI()
+		{
+
+			if (projectile.timeLeft == 300)
+            {
+				Main.PlaySound(SoundLoader.customSoundType, (int)Main.player[projectile.owner].position.X, (int)Main.player[projectile.owner].position.Y, mod.GetSoundSlot(SoundType.Custom, "Sounds/BombBombFlick"));
+			}
+
+			if (projectile.owner == Main.myPlayer && projectile.timeLeft <= 3)
+			{
+				projectile.tileCollide = false;
+				// Set to transparent. This projectile technically lives as  transparent for about 3 frames
+				projectile.alpha = 255;
+				// change the hitbox size, centered about the original projectile center. This makes the projectile damage enemies during the explosion.
+				projectile.position = projectile.Center;
+				//projectile.position.X = projectile.position.X + (float)(projectile.width / 2);
+				//projectile.position.Y = projectile.position.Y + (float)(projectile.height / 2);
+				projectile.width = 150;
+				projectile.height = 150;
+				projectile.Center = projectile.position;
+				//projectile.position.X = projectile.position.X - (float)(projectile.width / 2);
+				//projectile.position.Y = projectile.position.Y - (float)(projectile.height / 2);
+			}
+			else
+			{
+				// Smoke and fuse dust spawn.
+				if (Main.rand.NextBool())
+				{
+					int dustIndex = Dust.NewDust(new Vector2(projectile.position.X, projectile.position.Y), projectile.width, projectile.height, 31, 0f, 0f, 100, default(Color), 1f);
+					Main.dust[dustIndex].scale = 0.1f + (float)Main.rand.Next(5) * 0.1f;
+					Main.dust[dustIndex].fadeIn = 1.5f + (float)Main.rand.Next(5) * 0.1f;
+					Main.dust[dustIndex].noGravity = true;
+					Main.dust[dustIndex].position = projectile.Center + new Vector2(0f, (float)(-(float)projectile.height / 2)).RotatedBy((double)projectile.rotation, default(Vector2)) * 1.1f;
+					dustIndex = Dust.NewDust(new Vector2(projectile.position.X, projectile.position.Y), projectile.width, projectile.height, 6, 0f, 0f, 100, default(Color), 1f);
+					Main.dust[dustIndex].scale = 1f + (float)Main.rand.Next(5) * 0.1f;
+					Main.dust[dustIndex].noGravity = true;
+					Main.dust[dustIndex].position = projectile.Center + new Vector2(0f, (float)(-(float)projectile.height / 2 - 6)).RotatedBy((double)projectile.rotation, default(Vector2)) * 1.1f;
+				}
+			}
+			return;
+		}
+
+		public override void Kill(int timeLeft)
+        {
+            DevilFruitMod.hands--;
+
+			// Play explosion sound
+			Main.PlaySound(SoundID.Item14, projectile.position);
+			// Smoke Dust spawn
+			for (int i = 0; i < 50; i++)
+			{
+				int dustIndex = Dust.NewDust(new Vector2(projectile.position.X, projectile.position.Y), projectile.width, projectile.height, 31, 0f, 0f, 100, default(Color), 2f);
+				Main.dust[dustIndex].velocity *= 1.4f;
+			}
+			// Fire Dust spawn
+			for (int i = 0; i < 80; i++)
+			{
+				int dustIndex = Dust.NewDust(new Vector2(projectile.position.X, projectile.position.Y), projectile.width, projectile.height, 6, 0f, 0f, 100, default(Color), 3f);
+				Main.dust[dustIndex].noGravity = true;
+				Main.dust[dustIndex].velocity *= 5f;
+				dustIndex = Dust.NewDust(new Vector2(projectile.position.X, projectile.position.Y), projectile.width, projectile.height, 6, 0f, 0f, 100, default(Color), 2f);
+				Main.dust[dustIndex].velocity *= 3f;
+			}
+			// Large Smoke Gore spawn
+			for (int g = 0; g < 2; g++)
+			{
+				int goreIndex = Gore.NewGore(new Vector2(projectile.position.X + (float)(projectile.width / 2) - 24f, projectile.position.Y + (float)(projectile.height / 2) - 24f), default(Vector2), Main.rand.Next(61, 64), 1f);
+				Main.gore[goreIndex].scale = 1.5f;
+				Main.gore[goreIndex].velocity.X = Main.gore[goreIndex].velocity.X + 1.5f;
+				Main.gore[goreIndex].velocity.Y = Main.gore[goreIndex].velocity.Y + 1.5f;
+				goreIndex = Gore.NewGore(new Vector2(projectile.position.X + (float)(projectile.width / 2) - 24f, projectile.position.Y + (float)(projectile.height / 2) - 24f), default(Vector2), Main.rand.Next(61, 64), 1f);
+				Main.gore[goreIndex].scale = 1.5f;
+				Main.gore[goreIndex].velocity.X = Main.gore[goreIndex].velocity.X - 1.5f;
+				Main.gore[goreIndex].velocity.Y = Main.gore[goreIndex].velocity.Y + 1.5f;
+				goreIndex = Gore.NewGore(new Vector2(projectile.position.X + (float)(projectile.width / 2) - 24f, projectile.position.Y + (float)(projectile.height / 2) - 24f), default(Vector2), Main.rand.Next(61, 64), 1f);
+				Main.gore[goreIndex].scale = 1.5f;
+				Main.gore[goreIndex].velocity.X = Main.gore[goreIndex].velocity.X + 1.5f;
+				Main.gore[goreIndex].velocity.Y = Main.gore[goreIndex].velocity.Y - 1.5f;
+				goreIndex = Gore.NewGore(new Vector2(projectile.position.X + (float)(projectile.width / 2) - 24f, projectile.position.Y + (float)(projectile.height / 2) - 24f), default(Vector2), Main.rand.Next(61, 64), 1f);
+				Main.gore[goreIndex].scale = 1.5f;
+				Main.gore[goreIndex].velocity.X = Main.gore[goreIndex].velocity.X - 1.5f;
+				Main.gore[goreIndex].velocity.Y = Main.gore[goreIndex].velocity.Y - 1.5f;
+			}
+			// reset size to normal width and height.
+			projectile.position.X = projectile.position.X + (float)(projectile.width / 2);
+			projectile.position.Y = projectile.position.Y + (float)(projectile.height / 2);
+			projectile.width = 12;
+			projectile.height = 12;
+			projectile.position.X = projectile.position.X - (float)(projectile.width / 2);
+			projectile.position.Y = projectile.position.Y - (float)(projectile.height / 2);
+
+			{
+				int explosionRadius = 3;
+
+				int minTileX = (int)(projectile.position.X / 16f - (float)explosionRadius);
+				int maxTileX = (int)(projectile.position.X / 16f + (float)explosionRadius);
+				int minTileY = (int)(projectile.position.Y / 16f - (float)explosionRadius);
+				int maxTileY = (int)(projectile.position.Y / 16f + (float)explosionRadius);
+				if (minTileX < 0)
+				{
+					minTileX = 0;
+				}
+				if (maxTileX > Main.maxTilesX)
+				{
+					maxTileX = Main.maxTilesX;
+				}
+				if (minTileY < 0)
+				{
+					minTileY = 0;
+				}
+				if (maxTileY > Main.maxTilesY)
+				{
+					maxTileY = Main.maxTilesY;
+				}
+				bool canKillWalls = false;
+				for (int x = minTileX; x <= maxTileX; x++)
+				{
+					for (int y = minTileY; y <= maxTileY; y++)
+					{
+						float diffX = Math.Abs((float)x - projectile.position.X / 16f);
+						float diffY = Math.Abs((float)y - projectile.position.Y / 16f);
+						double distance = Math.Sqrt((double)(diffX * diffX + diffY * diffY));
+						if (distance < (double)explosionRadius && Main.tile[x, y] != null && Main.tile[x, y].wall == 0)
+						{
+							canKillWalls = true;
+							break;
+						}
+					}
+				}
+				AchievementsHelper.CurrentlyMining = true;
+				for (int i = minTileX; i <= maxTileX; i++)
+				{
+					for (int j = minTileY; j <= maxTileY; j++)
+					{
+						float diffX = Math.Abs((float)i - projectile.position.X / 16f);
+						float diffY = Math.Abs((float)j - projectile.position.Y / 16f);
+						double distanceToTile = Math.Sqrt((double)(diffX * diffX + diffY * diffY));
+						if (distanceToTile < (double)explosionRadius)
+						{
+							bool canKillTile = true;
+							if (Main.tile[i, j] != null && Main.tile[i, j].active())
+							{
+								canKillTile = true;
+								if (Main.tileDungeon[(int)Main.tile[i, j].type] || Main.tile[i, j].type == 88 || Main.tile[i, j].type == 21 || Main.tile[i, j].type == 26 || Main.tile[i, j].type == 107 || Main.tile[i, j].type == 108 || Main.tile[i, j].type == 111 || Main.tile[i, j].type == 226 || Main.tile[i, j].type == 237 || Main.tile[i, j].type == 221 || Main.tile[i, j].type == 222 || Main.tile[i, j].type == 223 || Main.tile[i, j].type == 211 || Main.tile[i, j].type == 404)
+								{
+									canKillTile = false;
+								}
+								if (!Main.hardMode && Main.tile[i, j].type == 58)
+								{
+									canKillTile = false;
+								}
+								if (!TileLoader.CanExplode(i, j))
+								{
+									canKillTile = false;
+								}
+								if (canKillTile)
+								{
+									WorldGen.KillTile(i, j, false, false, false);
+									if (!Main.tile[i, j].active() && Main.netMode != NetmodeID.SinglePlayer)
+									{
+										NetMessage.SendData(MessageID.TileChange, -1, -1, null, 0, (float)i, (float)j, 0f, 0, 0, 0);
+									}
+								}
+							}
+							if (canKillTile)
+							{
+								for (int x = i - 1; x <= i + 1; x++)
+								{
+									for (int y = j - 1; y <= j + 1; y++)
+									{
+										if (Main.tile[x, y] != null && Main.tile[x, y].wall > 0 && canKillWalls && WallLoader.CanExplode(x, y, Main.tile[x, y].wall))
+										{
+											WorldGen.KillWall(x, y, false);
+											if (Main.tile[x, y].wall == 0 && Main.netMode != NetmodeID.SinglePlayer)
+											{
+												NetMessage.SendData(MessageID.TileChange, -1, -1, null, 2, (float)x, (float)y, 0f, 0, 0, 0);
+											}
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+				AchievementsHelper.CurrentlyMining = false;
+			}
+		}
+
+    }
+}
+
---
+++ DevilFruitMod/DFTemplateDFTemplateFruit/DFTemplateDFTemplateAttack2.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.GameContent.Achievements;
+using Terraria.ID;
+
+namespace DevilFruitMod.DFTemplateDFTemplateFruit
+{
+    public class DFTemplateDFTemplateAttack2 : ModProjectile
+    {
+        public override void SetDefaults()
+        {
+            projectile.width = 12;
+            projectile.height = 12;
+            projectile.aiStyle = 1;
+            projectile.friendly = true;
+            projectile.ranged = true;
+            projectile.alpha = 128;
+            projectile.timeLeft = 50;
+			projectile.penetrate = -1;
+		}
+
+
+		public override void ModifyHitNPC(NPC target, ref int damage, ref float knockback, ref bool crit, ref int hitDirection)
+		{
+
+		}
+
+		public override bool OnTileCollide(Vector2 oldVelocity)
+		{
+			projectile.timeLeft = 1;
+			return false;
+		}
+
+
+		public override void AI()
+		{
+			
+		}
+
+		public override void Kill(int timeLeft)
+		{
+			DevilFruitMod.hands--;
+		}
+    }
+}
+
